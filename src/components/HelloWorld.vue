7b1496257b495e376015f5078e27e8204b381062
Hyeonmin Seon <hyunmui@outlook.kr>
2022-06-21 00:10:21 +0900

    컴포넌트로 빼기

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,55 +5,24 @@
     Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 
-  <div>
-    <input type="text" v-model="keyword" />
-  </div>
-
-  <ul class="post-list">
-    <li v-for="(post, index) in searchPosts" :key="index">
-      <a :href="post.url">
-        <h2>{{ post.title }}</h2>
-      </a>
-      <div class="post-info">
-        <div class="post-date">{{ post.createdAt }}</div>
-        <div class="post-author">
-          <a href="mailto:hyunmui@outlook.kr">홍길동</a>
-        </div>
-      </div>
-      <div class="post-summary">
-        <a :href="post.url">{{ post.summary }}</a>
-      </div>
-    </li>
-  </ul>
+  <PostList></PostList>
 </template>
 
 <script>
+import PostList from "./PostList.vue";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String,
   },
+  components: {
+    PostList,
+  },
   data() {
     return {
-      keyword: "",
       count: 0,
-      posts: [],
     };
-  },
-  mounted() {
-    fetch("/list.json")
-      .then((response) => response.json())
-      .then((posts) => (this.posts = posts));
-  },
-  computed: {
-    searchPosts() {
-      return this.posts.filter(
-        (post) =>
-          this.keyword == "" ||
-          post.summary.includes(this.keyword) ||
-          post.title.includes(this.keyword)
-      );
-    },
   },
 };
 </script>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,51 @@
-<template></template>
+<template>
+  <div>
+    <input type="text" v-model="keyword" />
+  </div>
+
+  <ul class="post-list">
+    <li v-for="(post, index) in searchPosts" :key="index">
+      <a :href="post.url">
+        <h2>{{ post.title }}</h2>
+      </a>
+      <div class="post-info">
+        <div class="post-date">{{ post.createdAt }}</div>
+        <div class="post-author">
+          <a href="mailto:hyunmui@outlook.kr">홍길동</a>
+        </div>
+      </div>
+      <div class="post-summary">
+        <a :href="post.url">{{ post.summary }}</a>
+      </div>
+    </li>
+  </ul>
+</template>
 
 <script>
-export default {};
+export default {
+  name: "PostList",
+  data() {
+    return {
+      keyword: "",
+      posts: [],
+    };
+  },
+  mounted() {
+    fetch("/list.json")
+      .then((response) => response.json())
+      .then((posts) => (this.posts = posts));
+  },
+  computed: {
+    searchPosts() {
+      return this.posts.filter(
+        (post) =>
+          this.keyword == "" ||
+          post.summary.includes(this.keyword) ||
+          post.title.includes(this.keyword)
+      );
+    },
+  },
+};
 </script>
 
 <style>
